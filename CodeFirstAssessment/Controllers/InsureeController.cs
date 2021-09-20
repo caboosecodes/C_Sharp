6d62a2291d4509c6b8929c8cdb77ac43f97700d1
caboosecodes <ricardolopez413@hotmail.com>
2021-09-20 08:43:14 -0700

    1st attempt at code first car insurance

---
+++ CodeFirstAssessment/Controllers/InsureeController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirstAssessment.DAL;
+using CodeFirstAssessment.Models;
+
+namespace CodeFirstAssessment.Controllers
+{
+    [Authorize]
+    public class InsureeController : Controller
+    {
+        private CodeFirstAssessmentContext db = new CodeFirstAssessmentContext();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                insuree.Quote = calculateQuote(insuree);
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        public static decimal calculateQuote(Insuree insuree)
+        {
+
+            decimal rate = 50.0M;
+
+            DateTime now = DateTime.Today;
+            var age = now.Year - insuree.DateOfBirth.Year;
+            if (age <= 18)
+            {
+                rate += 100.0M;
+            }
+            if (age >= 19 && age <= 25)
+            {
+                rate += 50.0M;
+            }
+            if (age > 25)
+            {
+                rate += 25.0M;
+            }
+            if (insuree.CarYear < 2000)
+            {
+                rate += 25.0M;
+            }
+            if (insuree.CarYear > 2015)
+            {
+                rate += 25.0M;
+            }
+            if (insuree.CarMake.ToLower() == "porche")
+            {
+                rate += 25.0M;
+            }
+            if (insuree.CarMake.ToLower() == "porche" && insuree.CarModel.ToLower() == "911 carrera")
+            {
+                rate += 25.0M;
+            }
+            if (insuree.SpeedingTickets > 0)
+            {
+                rate += (insuree.SpeedingTickets * 10);
+            }
+            if (insuree.DUI)
+            {
+                rate += (rate * .25M);
+            }
+            if (insuree.CoverageType)
+            {
+                rate += (rate * .5M);
+            }
+            insuree.Quote = rate;
+            return insuree.Quote;
+        }
+        
+
+            // GET: Insuree/Edit/5
+            [Authorize(Roles = "Admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Delete/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CodeFirstAssessment/DAL/CodeFirstAssessmentContext.cs
@@ -0,0 +1,24 @@
+﻿using CodeFirstAssessment.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstAssessment.DAL
+{
+    public class CodeFirstAssessmentContext : DbContext
+    {
+        public CodeFirstAssessmentContext() : base("CodeFirstAssessmentContext")
+        {
+        }
+
+        public DbSet<Insuree> Insurees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
